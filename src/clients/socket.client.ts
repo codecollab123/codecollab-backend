a215a30db9160d32c3103caa5c754eed81475d26
tushar2708 <rtushar2708@gmail.com>
2025-02-06 23:13:28 +0530

    test socket

--- src/clients/socket.client.ts
+++ src/clients/socket.client.ts
@@ -7,9 +7,26 @@ export namespace SocketClient {
     // Create the socket.io server
     const io = new Server(fastify.server);  // Use `new socketIO.Server(...)
 
+    const userSocketMap = {};
+    function getAllConnectedClients(room_id: string) {
+      return Array.from(io.sockets.adapter.rooms.get(room_id) || []).map((socketId) => {
+        return {
+          socketId,
+          userName: userSocketMap[socketId],
+        };
+      });
+    };
+
     // Handle new WebSocket connections
     io.on("connection", (socket) => {
-      logger.info("A user connected");
+      logger.info(`A user connected ${socket.id}`);
+
+      socket.on('join', ({ room_id, userName }) => {
+        userSocketMap[socket.id] = userName;
+        socket.join(room_id);
+        const clients = getAllConnectedClients(room_id);
+        logger.info(clients);
+      })
 
       // Handle message from the client
       socket.on("message", (msg) => {
